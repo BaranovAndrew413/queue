a21f129345cb79bf4149541836ba904a0ed26e50
BaranovAndrew413 <andrey-baranov1@mail.ru>
2019-12-18 00:13:35 +0400

    s

--- test_stack.cpp
+++ test_stack.cpp
@@ -1,73 +1,73 @@
-#include "..\mp2-lab2-stack\TStack.h"
+#include "..\queue\TQueue.h"
 
 #include "gtest.h"
 
-TEST(TStack, can_create_TStack_with_positive_length)
+TEST(TQueue, can_create_TQueue_with_positive_length)
 {
-  ASSERT_NO_THROW(TStack<int> ts(3));                    //ASSERT_N_THROW (Œ∆»ƒ¿Õ»≈, ◊“Œ »— Àﬁ◊≈Õ»≈ Õ≈ ¬€À≈«≈“
-}                                                       //A
+	ASSERT_NO_THROW(TQueue<int> ts(3));                    
+}                                                       
 
 
-TEST(TStack, can_create_TStack_with_negative_length)
+TEST(TStack, can_create_TQueue_with_negative_length)
 {
-	ASSERT_ANY_THROW(TStack<int> ts(-3));
+	ASSERT_ANY_THROW(TQueue<int> ts(-3));
 }
 
-TEST(TStack, vzyatie_is_pustogo_TStack)
+TEST(TQueue, vzyatie_is_pustogo_TStack)
 {
-	TStack<int>ts(1);
+	TQueue<int>ts(1);
 	ASSERT_ANY_THROW(ts.Pop());
 }
 
 TEST(TStack, vzyatie_verhnego_elementa_is_TStack)
 {
-	TStack<int>ts(2);
+	TQueue<int>ts(2);
 	ts.Push(1);
 	ts.Push(2);
 	ASSERT_EQ(ts.Top(),2);
 }
 
-TEST(TStack, ubrat_verhniy_element_is_TStack)
+TEST(TQueue, ubrat_verhniy_element_is_TQueue)
 {
-	TStack<int>ts(2);
+	TQueue<int>ts(2);
 	ts.Push(1);
 	ts.Push(2);
 	ts.Pop();
 	ASSERT_EQ(ts.Top(),1);
 }
 
-TEST(TStack, vzyatie_verhnego_elementa_is_pystogo_TStack)
+TEST(TQueue, vzyatie_verhnego_elementa_is_pystogo_TQueue)
 {
-	TStack<int>ts(0);
+	TQueue<int>ts(0);
 	ASSERT_ANY_THROW(ts.Top());
 }
 
-TEST(TStack, ybrat_element_is_pystogo_TStack)
+TEST(TQueue, ybrat_element_is_pystogo_TStack)
 {
-	TStack<int>ts(0);
+	TQueue<int>ts(0);
 	ASSERT_ANY_THROW(ts.Pop());
 }
 
-TEST(TStack, can_create_copied_TStack)
+TEST(TQueue, can_create_copied_TQueue)
 {
-	TStack<int> m(5);
-	ASSERT_NO_THROW(TStack<int> m1(m));
+	TQueue<int> m(5);
+	ASSERT_NO_THROW(TQueue<int> m1(m));
 }
 
-TEST(TStack, copied_TStack_is_equal_to_source_one)
+TEST(TStack, copied_TQueue_is_equal_to_source_one)
 {
-	TStack <int> m(4);
+	TQueue <int> m(4);
 	m.Push(1);
-	TStack <int> m1(m);
+	TQueue <int> m1(m);
 	int a = m.Pop();
 	int b = m1.Pop();
 	EXPECT_EQ(a, b);
 }
 
-TEST(TStack, copied_TStack_has_its_own_memory)
+TEST(TQueue, copied_TQueue_has_its_own_memory)
 {
-	TStack<int> m1(4);
-	TStack<int> m2(4);
+	TQueue<int> m1(4);
+	TQueue<int> m2(4);
 
 	m1.Push(1);
 	m1.Push(2);
